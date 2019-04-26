6b57350dfaa90dba951fbb88058e9c7219368906
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-26 12:30:47 +0000

    Deploy Michael-F-Bryan/daily-programmer to github.com/Michael-F-Bryan/daily-programmer.git:gh-pages

--- implementors/core/trait.Challenge.js
+++ implementors/core/trait.Challenge.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["easy_374"] = [{text:"impl <a class=\"trait\" href=\"core/trait.Challenge.html\" title=\"trait core::Challenge\">Challenge</a> for <a class=\"struct\" href=\"easy_374/struct.Easy374.html\" title=\"struct easy_374::Easy374\">Easy374</a>",synthetic:false,types:["easy_374::Easy374"]},];
 implementors["easy_375"] = [{text:"impl <a class=\"trait\" href=\"core/trait.Challenge.html\" title=\"trait core::Challenge\">Challenge</a> for <a class=\"struct\" href=\"easy_375/struct.Easy375.html\" title=\"struct easy_375::Easy375\">Easy375</a>",synthetic:false,types:["easy_375::Easy375"]},];
 implementors["intermediate_375"] = [{text:"impl <a class=\"trait\" href=\"core/trait.Challenge.html\" title=\"trait core::Challenge\">Challenge</a> for <a class=\"struct\" href=\"intermediate_375/struct.Intermediate375.html\" title=\"struct intermediate_375::Intermediate375\">Intermediate375</a>",synthetic:false,types:["intermediate_375::Intermediate375"]},];
 
